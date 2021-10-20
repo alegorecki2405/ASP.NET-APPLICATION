268327442ce7c0412ea8259276aeed12a4b06609
Aleksander Gorecki <alegorecki@gmail.com>
2021-10-20 22:28:25 +0200

    commit

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -33,6 +33,5 @@ namespace WebApplication1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        Console.Writeline("dddddd");
     }
 }
